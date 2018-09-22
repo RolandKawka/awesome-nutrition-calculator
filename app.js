744acd0c785a6994d80356ac7644cdbd048bf93e
Roland Kawka <roland.kawka@netins.it>
2018-09-22 23:30:10 +0200

    add product model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
+require('./models/Product');
 
 const app = express();
 
@@ -10,6 +11,17 @@ app.use(morgan('combined'));
 
 mongoose.connect(process.env.DB_URI);
 
+// const Product = mongoose.model('Product');
+// const newProduct = new Product({
+//     name: 'rice',
+//     fat: 1,
+//     carbs: 1,
+//     proteins: 1,
+//     calories: 1,
+// });
+
+// newProduct.save();
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.log('App listening on port 3000!'));
---
+++ models/Product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    created: {
+        type: Date,
+        default: Date.now,
+    },
+    name: {
+        type: String,
+        unique: true,
+        required: 'You must supply a name!',
+    },
+    proteins: {
+        type: Number,
+        required: 'You must supply proteins!',
+        min: 0,
+        max: 100,
+    },
+    carbs: {
+        type: Number,
+        required: 'You must supply carbs!',
+        min: 0,
+        max: 100,
+    },
+    fat: {
+        type: Number,
+        required: 'You must supply fat!',
+        min: 0,
+        max: 100,
+    },
+    calories: {
+        type: Number,
+        required: 'You must supply calories!',
+        min: 0,
+        max: 100,
+    },
+});
+
+module.exports = mongoose.model('Product', productSchema);
