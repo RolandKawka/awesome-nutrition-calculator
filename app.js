f4949a1a23d463f17956138d38cd98ccd750f43d
Roland Kawka <roland.kawka@netins.it>
2018-09-22 23:05:52 +0200

    add cors and morgan

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+
 const app = express();
+
+app.use(cors());
+app.use(morgan('combined'));
 
 mongoose.connect(process.env.DB_URI);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
