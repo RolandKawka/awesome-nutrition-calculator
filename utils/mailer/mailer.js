897600d96b0be92666313992d70d0eaa707da422
Hubert Strychalski <hubert.strychalski@netins.academy>
2018-09-23 18:23:24 +0200

    added mailer

---
+++ utils/mailer/mailer.js
@@ -0,0 +1,30 @@
+const httpStatus = require('./../http/httpStatusCodes');
+
+function sendEmail(options) {
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+        to: options.recipient,
+        from: process.env.SENGRID_EMAIL_FROM,
+        subject: options.subject,
+        text: options.text,
+        html: options.html,
+    };
+
+    let response = {};
+    sgMail
+        .send(msg)
+        .then(() => {
+            response.statusCode = httpStatus.OK;
+            response.message ='Email message has been sent successfully';
+        })
+        .catch((error) => {
+            response.statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+            response.message ='Error while sending email message';
+        });
+
+    return response;
+}
+
+module.exports = sendEmail;
