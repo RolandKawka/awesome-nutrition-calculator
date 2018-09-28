c0b35b032b17224cbd8c9cf85b162abc286f7cd7
Hubert Strychalski <hubert.strychalski@netins.academy>
2018-09-28 16:13:44 +0200

    add separate files for routes

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const morgan = require('morgan');
+const routes = require('./routes/index');
 require('./models/Product');
 require('./models/User');
-const seedDb = require('./seeds');
 const schema = require('./schema');
 
 const register = require('./controllers/register');
@@ -21,11 +20,7 @@ app.use(bodyParser.json());
 
 mongoose.connect(process.env.DB_URI);
 
-
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/seedDb', seedDb);
-app.post('/signin', (req, res) => signin.signinAuthentication(req, res, bcrypt));
-app.post('/register', (req, res) => register.handleRegister(req, res, bcrypt));
+app.use('/', routes);
 
 app.use('/graphql', graphqlHTTP({
     schema,
